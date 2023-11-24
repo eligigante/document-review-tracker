850e76d8a83efb9a42ae29a1febb4f31880340f1
Hans Rafael Daligdig <2220154@slu.edu.ph>
2023-11-24 20:12:19 +0800

    adjustments in html, css, and js

--- ver3/user/script.js
+++ ver3/user/script.js
@@ -1,26 +1,27 @@
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu.top li a');
 const section = document.querySelector("section"),
-overlay = document.querySelector(".overlay"),
-showBtn = document.querySelector(".logout"),
-closeBtn = document.querySelector(".close-btn");
+	overlay = document.querySelector(".overlay"),
+	showBtn = document.querySelector(".logout"),
+	closeBtn = document.querySelector(".close-btn");
+const searchButton = document.querySelector('#content nav form .form-input button');
+const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
+const searchForm = document.querySelector('#content nav form');
+// TOGGLE SIDEBAR
+const menuBar = document.querySelector('#content nav .bx.bx-menu');
+const sidebar = document.getElementById('sidebar');
+const switchMode = document.getElementById('switch-mode');
 
-allSideMenu.forEach(item=> {
+allSideMenu.forEach(item => {
 	const li = item.parentElement;
 
 	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
+		allSideMenu.forEach(i => {
 			i.parentElement.classList.remove('active');
 		})
 		li.classList.add('active');
 	})
 });
 
-
-
-
-// TOGGLE SIDEBAR
-const menuBar = document.querySelector('#content nav .bx.bx-menu');
-const sidebar = document.getElementById('sidebar');
 
 menuBar.addEventListener('click', function () {
 	sidebar.classList.toggle('hide');
@@ -31,26 +32,18 @@ menuBar.addEventListener('click', function () {
 showBtn.addEventListener("click", () => section.classList.add("active"));
 
 overlay.addEventListener("click", () =>
-  section.classList.remove("active")
+	section.classList.remove("active")
 );
 
 closeBtn.addEventListener("click", () =>
-  section.classList.remove("active")
+	section.classList.remove("active")
 );
 
-
-
-
-
-const searchButton = document.querySelector('#content nav form .form-input button');
-const searchButtonIcon = document.querySelector('#content nav form .form-input button .bx');
-const searchForm = document.querySelector('#content nav form');
-
 searchButton.addEventListener('click', function (e) {
-	if(window.innerWidth < 576) {
+	if (window.innerWidth < 576) {
 		e.preventDefault();
 		searchForm.classList.toggle('show');
-		if(searchForm.classList.contains('show')) {
+		if (searchForm.classList.contains('show')) {
 			searchButtonIcon.classList.replace('bx-search', 'bx-x');
 		} else {
 			searchButtonIcon.classList.replace('bx-x', 'bx-search');
@@ -58,44 +51,87 @@ searchButton.addEventListener('click', function (e) {
 	}
 })
 
-
-
-
-
-if(window.innerWidth < 768) {
+if (window.innerWidth < 768) {
 	sidebar.classList.add('hide');
-} else if(window.innerWidth > 576) {
+} else if (window.innerWidth > 576) {
 	searchButtonIcon.classList.replace('bx-x', 'bx-search');
 	searchForm.classList.remove('show');
 }
 
-
 window.addEventListener('resize', function () {
-	if(this.innerWidth > 576) {
+	if (this.innerWidth > 576) {
 		searchButtonIcon.classList.replace('bx-x', 'bx-search');
 		searchForm.classList.remove('show');
 	}
 })
 
-
-
-const switchMode = document.getElementById('switch-mode');
-
 switchMode.addEventListener('change', function () {
-	if(this.checked) {
+	if (this.checked) {
 		document.body.classList.add('dark');
 	} else {
 		document.body.classList.remove('dark');
 	}
 })
 
-
 //SPA 
+function loadContent(page) {
+	fetch(`content.php?page=${page}`)
+		.then(response => response.text())
+		.then(data => document.getElementById('tite').innerHTML = data)
+		.catch(error => console.error('Error:', error));
+}
 
 
-  function loadContent(page) {
-    fetch(`content.php?page=${page}`)
-      .then(response => response.text())
-      .then(data => document.getElementById('tite').innerHTML = data)
-      .catch(error => console.error('Error:', error));
+// Upload File
+function readFile(input) {
+	if (input.files && input.files[0]) {
+	  var reader = new FileReader();
+  
+	  reader.onload = function(e) {
+		var htmlPreview =
+		  '<img width="200" src="' + e.target.result + '" />' +
+		  '<p>' + input.files[0].name + '</p>';
+		var wrapperZone = $(input).parent();
+		var previewZone = $(input).parent().parent().find('.preview-zone');
+		var boxZone = $(input).parent().parent().find('.preview-zone').find('.box').find('.box-body');
+  
+		wrapperZone.removeClass('dragover');
+		previewZone.removeClass('hidden');
+		boxZone.empty();
+		boxZone.append(htmlPreview);
+	  };
+  
+	  reader.readAsDataURL(input.files[0]);
+	}
   }
+  
+  function reset(e) {
+	e.wrap('<form>').closest('form').get(0).reset();
+	e.unwrap();
+  }
+  
+  $(".dropzone").change(function() {
+	readFile(this);
+  });
+  
+  $('.dropzone-wrapper').on('dragover', function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	$(this).addClass('dragover');
+  });
+  
+  $('.dropzone-wrapper').on('dragleave', function(e) {
+	e.preventDefault();
+	e.stopPropagation();
+	$(this).removeClass('dragover');
+  });
+  
+  $('.remove-preview').on('click', function() {
+	var boxZone = $(this).parents('.preview-zone').find('.box-body');
+	var previewZone = $(this).parents('.preview-zone');
+	var dropzone = $(this).parents('.form-group').find('.dropzone');
+	boxZone.empty();
+	previewZone.addClass('hidden');
+	reset(dropzone);
+  });
+  
