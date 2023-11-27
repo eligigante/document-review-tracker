c5b5fa2a01a1657ed7264575a4c37a147f51fc92
amzapanta <142793396+amzapanta@users.noreply.github.com>
2023-11-27 21:52:19 +0800

    Update Node.JS files

--- node/login.js
+++ node/login.js
@@ -1,17 +1,127 @@
-const mysql = require('mysql');
 const server = require('./server');
 const express = require('express');
 const session = require('express-session');
+const path = require('path');
+const db = require('./db');
+const cookie = require('cookie-parser');
+const { request } = require('http');
+
+const connection = db.connectDatabase();
+db.getConnection(connection);
 
 const app = express();
 
 app.use(session({
     secret : 'document-tracker-key',
     resave : true,
-    saveUninitialized : true
+    saveUninitialized : true,
+    cookie: {maxAge: 86400}
   }));  
 
-server.startServer();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));;
+app.use(cookie());
+app.use(express.static(path.dirname('public')));
+
+app.get('/', function(request, response) {
+  response.sendFile(path.basename('public/index.html'))
+})
+
+app.post('/login', (request, response) => {
+  const id = request.body.accountID;
+  const password = request.body.password;
+
+  if (id, password) {
+    const check = db.checkUser(id);
+    if (check) {
+      connection.query(db.verifyCredentials, [id, [password], function(err, result, fields) {
+        if (err) throw err;
+        if (result.length > 0) {
+          request.session.accountID = id;
+          request.session.loggedIn = true;
+          console.log("Successfully logged in.");
+          response.redirect('/home');
+        }
+        else {
+          console.log("Incorrect password.");
+          response.redirect('/');
+        }
+      }])
+    }
+    else {
+      console.log("User does not exist.");
+      response.redirect('/');
+    }
+  }
+})
+
+app.get('/home', function(request, response) {
+  if (request.session.loggedIn) {
+    response.sendFile(path.basename('public/home.html'));
+    console.log("Welcome back, User!")
+    response.end();
+  }
+  else {
+    console.log("Please login.")
+    response.redirect('/');
+    response.end();
+  }
+})
+
+server.startServer(app);
+
+
+  // if (id, password) {
+  //   connection.query(db.verifyUser, [id], function(err, result, fields) {
+  //     if (err) throw err;
+  //     if (result.length > 0) {
+  //       request.session.loggedin = true;
+  //       request.session.username = username;
+  //       console.log ("User Exists");
+  //     } 
+  //     else {
+  //       console.log ("User does not exist")
+  //     }
+  //   })
+  // }
+//   if
+// }))
+
+
+// app.get('/', function(request, response) {
+//     response.sendFile('/document-review-tracker-main/ver3/user/index.php');
+// });
+
+// app.post('/auth', function(request, response) {
+
+//   let username = request.body.username;
+//   let password = request.body.password;
+
+//   if (username && password) {
+//     var sql = 'SELECT user_ID, password FROM user WHERE user_ID = ? AND password = ?';
+//     const result = db.query(sql, [username, password], function(error, results, fields) {
+//       if (error) throw error;
+//             if (results.length > 0) {
+//                 request.session.loggedin = true;
+//                 request.session.username = username;
+//                 response.redirect('/home');
+//             } else {
+//                 response.send('Incorrect Username and/or Password!');
+//             }           
+//             response.end();
+//     });
+//     } else {
+//         response.send('Please enter Username and Password!');
+//         response.end();
+//     }
+// });
+
+
+//  app.listen('8001', () => {
+//         console.log('Server has connected to port 8001');
+//     })
+
+
 
 
 
--- node/queries.js
+++ node/queries.js
@@ -1,89 +1,103 @@
 const mysql = require('mysql');
 
-function getUser(id) {
-    if (checkUser(id) ) {
-        const result = db.query('SELECT * FROM user WHERE user_ID = ?', [id]);
-        const rows = result[0];
-        return rows;
-    }
-    else {
-        return "";
-    }   
-}
+const verifyCredentials = 'SELECT * FROM user WHERE user_ID = ? AND password = ?';
 
-function insertUser(user_ID, email, password, last_Name, first_Name, middle_Name, department_ID, position, status) {
-    var sql = 'INSERT INTO user (user_ID, email, password, last_Name, first_Name, middle_Name, department_ID, position, status)' +
-    'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    db.query(sql, [user_ID, email, password, last_Name, first_Name, middle_Name, department_ID, position, status])
-    console.log("User data has been inserted into the database.")
-}
 
-function deleteUser(id) {
-    var sql = 'DELETE FROM user WHERE user.`user_ID` = ?';
-    db.query(sql, [id]);
-    console.log("User had been deleted from the database.");    
-}
+// function getUser(id) {
+//     if (checkUser(id) ) {
+//         const result = db.query('SELECT * FROM user WHERE user_ID = ?', [id]);
+//         const rows = result[0];
+//         return rows;
+//     }
+//     else {
+//         return "";
+//     }   
+// }
 
-function checkIfOnline(id) {
-    var sql = 'SELECT user_ID FROM user WHERE user_ID=\'202302\' AND status=\'Online\'';
-    const result = db.query(sql, [id]);
-    return Boolean(result);
-}
+// function insertUser(user_ID, email, password, last_Name, first_Name, middle_Name, department_ID, position, status) {
+//     var sql = 'INSERT INTO user (user_ID, email, password, last_Name, first_Name, middle_Name, department_ID, position, status)' +
+//     'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+//     db.query(sql, [user_ID, email, password, last_Name, first_Name, middle_Name, department_ID, position, status])
+//     console.log("User data has been inserted into the database.")
+// }
+
+// function deleteUser(id) {
+//     var sql = 'DELETE FROM user WHERE user.`user_ID` = ?';
+//     db.query(sql, [id]);
+//     console.log("User had been deleted from the database.");    
+// }
+
+// function checkIfOnline(id) {
+//     var sql = 'SELECT user_ID FROM user WHERE user_ID=\'202302\' AND status=\'Online\'';
+//     const result = db.query(sql, [id]);
+//     return Boolean(result);
+// }
 
 function checkUser(id) {
     var sql = 'SELECT * FROM user WHERE user_ID = ?';
-    const result = db.query(sql, [id]);
-    return Boolean(result)
+    db.query(sql, [id], function(error, result, fields) {
+        if (result.length > 0) {
+            return true
+        }
+        else {
+            return false
+        }
+    });
 }
 
-function checkPassword (id, password) {
-    var sql = 'SELECT user_ID, password FROM user WHERE user_ID = ? AND password = ?';
-    const result = db.query(sql, [id, password]);
-    return Boolean(result);
-}
-
-function getUserDocuments (id) {
-    var sql = 'SELECT * FROM document_details WHERE user_ID = ?';
-    const result = db.query(sql, [id]);
-    const rows = result[0];
-    return rows;
-}
-
-function getDocumentLog (doc_ID) {
-    var sql = 'SELECT * FROM document_logs WHERE document_ID = ?';
-    const result = db.query(sql, [doc_ID]);
-    const rows = result[0];
-    return rows;
-}
-
-function checkReviewer (department_ID, position) {
-    var sql = 'SELECT department_ID, position FROM user WHERE department_ID = ? AND position = ?';
-    const result = db.query(sql, [department_ID, position]);
-    return Boolean(result);
-}
-
-function getReviewerDocuments (department_ID, position) {
-    if (checkReviewer(department_ID, position)) {
-        var sql = 'SELECT * FROM document_logs WHERE department_ID = ?';
-        const result = db.query(sql, [department_ID])
-        const rows = result[0];
-        return rows;
-    }
-    
-}
 
 module.exports = {
-    getUser, 
-    insertUser,
-    deleteUser,
-    checkIfOnline,
-    checkUser,
-    checkPassword,
-    getUserDocuments,
-    getDocumentLog,
-    checkReviewer,
-    getReviewerDocuments
+    verifyCredentials, 
+    checkUser
 }
+// function checkPassword (id, password) {
+//     var sql = 'SELECT user_ID, password FROM user WHERE user_ID = ? AND password = ?';
+//     const result = db.query(sql, [id, password]);
+//     return Boolean(result);
+// }
+
+// function getUserDocuments (id) {
+//     var sql = 'SELECT * FROM document_details WHERE user_ID = ?';
+//     const result = db.query(sql, [id]);
+//     const rows = result[0];
+//     return rows;
+// }
+
+// function getDocumentLog (doc_ID) {
+//     var sql = 'SELECT * FROM document_logs WHERE document_ID = ?';
+//     const result = db.query(sql, [doc_ID]);
+//     const rows = result[0];
+//     return rows;
+// }
+
+// function checkReviewer (department_ID, position) {
+//     var sql = 'SELECT department_ID, position FROM user WHERE department_ID = ? AND position = ?';
+//     const result = db.query(sql, [department_ID, position]);
+//     return Boolean(result);
+// }
+
+// function getReviewerDocuments (department_ID, position) {
+//     if (checkReviewer(department_ID, position)) {
+//         var sql = 'SELECT * FROM document_logs WHERE department_ID = ?';
+//         const result = db.query(sql, [department_ID])
+//         const rows = result[0];
+//         return rows;
+//     }
+    
+// }
+
+// module.exports = {
+//     getUser, 
+//     insertUser,
+//     deleteUser,
+//     checkIfOnline,
+//     checkUser,
+//     checkPassword,
+//     getUserDocuments,
+//     getDocumentLog,
+//     checkReviewer,
+//     getReviewerDocuments
+// }
 
 
 // console.log(insertUser(2220172, '2220172@slu.edu.ph', '123abc', 'Zapanta', 'Adrienne Marie', 'Banday', 10, 'President', 'online'));
