a4617d260dcd2eab90d52bf09b183e8a33a7b653
SinxLawliet <d.gabrielronquillo28@gmail.com>
2023-12-05 19:16:03 +0800

    Download blob to pdf

--- node/admin.js
+++ node/admin.js
@@ -7,6 +7,11 @@ const queries = require('./queries');
 const server = require('./server');
 const db = require('./db');
 const { request } = require('http');
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const connection = db.connectDatabase(mysql);
 db.getConnection(connection);
@@ -31,6 +36,11 @@ server.startServer(app);
 app.get('/', function(request, response) {
   response.sendFile(path.resolve(__dirname + '/../public/index.html'))
 })
+
+const tempFolderPath = path.resolve(__dirname, "../temp");
+if (!fs.existsSync(tempFolderPath)) {
+  fs.mkdirSync(tempFolderPath);
+}
 
 app.post('/login', (request, response) => {
   const id = request.body.accountID;
@@ -153,6 +163,68 @@ app.post('/edit_user', (request, response) => {
   console.log('User details successfully updated.');
   response.redirect('/manage_user');
 })
+
+app.get("/review_doc", (request, response) => {
+  if (request.session.loggedIn && request.session.role === "reviewer") {
+    connection.query(
+      "SELECT document_ID, user_ID, document_Title, copies, upload_Date, file FROM document_details",
+      (err, results) => {
+        if (err) throw err;
+        response.render("review_doc", { data: results });
+      }
+    );
+  } else {
+    response.redirect("/");
+  }
+});
+
+app.get("/pdfviewer", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../public/pdfviewer.html"));
+});
+
+app.get("/temp/document/:documentId.pdf", (req, res) => {
+  const documentId = req.params.documentId;
+
+  if (!documentId) {
+    return res.status(400).json({ error: "Invalid request" });
+  }
+
+  const filename = `document_${documentId}.pdf`;
+  const filePath = path.resolve(__dirname, "../temp", filename);
+
+  res.contentType("application/pdf");
+  res.sendFile(filePath);
+});
+
+app.get("/downloadAndConvert/:documentId", (req, res) => {
+  try {
+    console.log("Reviewing Document: " + documentId)
+    const documentId = req.params.documentId;
+
+    if (!documentId) {
+      return res.status(400).json({ error: "Invalid request" });
+    }
+    connection.query(
+      "SELECT file FROM document_details WHERE document_ID = ?",
+      [documentId],
+      (err, results) => {
+        if (err) throw err;
+
+        const blobData = results[0].file;
+
+        const filename = `../temp/document_${documentId}.pdf`;
+        fs.writeFileSync(filename, Buffer.from(blobData));
+
+        res.contentType("application/pdf");
+        res.sendFile(path.resolve(__dirname, filename));
+      }
+    );
+  } catch (error) {
+    console.error("Error downloading and converting Blob data:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 
 
   
