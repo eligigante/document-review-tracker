834093b6b73c451a84a5634d5cb81c54b75b01c3
Kibent123 <2224372@slu.edu.ph>
2023-12-14 00:31:34 +0800

    tracking

--- php/document.php
+++ php/document.php
@@ -19,18 +19,22 @@ $documents = get_docs($con, $userID);
 $docs = json_decode($documents, true);
 
 if ($docs !== null) {
+
+    $statusChanging = "";
+
     foreach ($docs as $doc) {
         switch ($doc['status']) {
-                    case 'pending':
-                        $statusChanging = 'status pending';
-                        break;
-                    case 'finished':
-                        $statusChanging = 'status completed';
-                        break;
-                    case 'rejected':
-                        $statusChanging = 'status denied';
-                        break;
-                }
+            case 'pending':
+                $statusChanging = 'status pending';
+                break;
+            case 'finished':
+                $statusChanging = 'status completed';
+                break;
+            case 'rejected':
+                $statusChanging = 'status denied';
+                break;
+        }
+
 
         echo '
         <tr>
@@ -46,9 +50,102 @@ if ($docs !== null) {
             <td>
                 <span class="' . $statusChanging . '">' . $doc['status'] . '</span>
             </td>
-            <td>
-                <span>" Currently at ' .$doc['depName'].' ' .$doc['department'].'"</span>
-            </td>
+            <td>';
+    
+
+               
+
+            switch ($doc['department']) {
+
+                
+
+                case '1':
+
+                    echo '<div class="delivery-tracking">
+                    <div class="word-container orange transition" id="circle1">OGRAA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle2">OVPAA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle3">OVPF</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle4">OLA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle5">OVPA</div>
+                  </div>';
+
+            break;
+                case '2':
+                    echo '<div class="delivery-tracking">
+                    <div class="word-container green transition" id="circle1">OGRAA</div>
+                    <div class="line green-line transition"></div>
+                    <div class="word-container orange transition" id="circle2">OVPAA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle3">OVPF</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle4">OLA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle5">OVPA</div>
+                  </div>';
+
+                    break;
+                case '3':
+
+                    echo '<div class="delivery-tracking">
+                    <div class="word-container green transition" id="circle1">OGRAA</div>
+                    <div class="line green-line transition"></div>
+                    <div class="word-container green transition" id="circle2">OVPAA</div>
+                    <div class="line green-line transition"></div>
+                    <div class="word-container orange transition" id="circle3">OVPF</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle4">OLA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle5">OVPA</div>
+                  </div>';
+
+                    break;
+                case '4':
+
+
+
+                    echo '<div class="delivery-tracking">
+                    <div class="word-container green transition" id="circle1">OGRAA</div>
+                    <div class="line green-line transition"></div>
+                    <div class="word-container green transition" id="circle2">OVPAA</div>
+                    <div class="line green-line transition"></div>
+                    <div class="word-container green transition" id="circle3">OVPF</div>
+                    <div class="line green-line transition"></div>
+                    <div class="word-container orange transition" id="circle4">OLA</div>
+                    <div class="line no-green transition"></div>
+                    <div class="word-container transition" id="circle5">OVPA</div>
+                  </div>';
+                 
+                    break;
+                    case '5':
+                      
+                        echo '<div class="delivery-tracking">
+                        <div class="word-container green transition" id="circle1">OGRAA</div>
+                        <div class="line green-line transition"></div>
+                        <div class="word-container green transition" id="circle2">OVPAA</div>
+                        <div class="line green-line transition"></div>
+                        <div class="word-container green transition" id="circle3">OVPF</div>
+                        <div class="line green-line transition"></div>
+                        <div class="word-container green transition" id="circle4">OLA</div>
+                        <div class="line green-line transition"></div>
+                        <div class="word-container orange transition" id="circle5">OVPA</div>
+                      </div>';
+                     
+
+                        break;
+                default:
+         
+                    echo '';
+            }
+
+
+            
+
+               
+           echo ' </td>
         </tr>';
     }
 } else {
@@ -57,3 +154,4 @@ if ($docs !== null) {
         </tr>';
 }
 ?>
+
